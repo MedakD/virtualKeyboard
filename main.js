ea35e2b83b78dcf9d34fb34f00690fcd7cf8698a
MedakD <timemedak@gmail.com>
2022-05-09 20:46:23 +0300

    feat render eng keyboard

--- main.js
+++ main.js
@@ -7,15 +7,16 @@ const keyboard = {
     const layouts = {
       rus: ['TODO'],
       eng: [
-        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-        "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-        "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
-        "space"
-      ]
+        '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+        'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
+        'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
+        'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&uarr;', 'Shift',
+        'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl']
     };
     return layouts[this.state.lang];
   },
+
+  keyRowsMap: [null, 14, 15, 13, 13, 9],
 
   init() {
     const main = document.createElement('div');
@@ -28,13 +29,25 @@ const keyboard = {
     const buttons = this.getLayout();
     const keyboardWrap = document.createElement('div');
     keyboardWrap.classList.add('keyboard_wrap');
-    buttons.forEach(button => {
-      const btnElement = document.createElement('button');
-      btnElement.innerHTML = button;
-      keyboardWrap.appendChild(btnElement);
+
+    this.keyRowsMap.reduce((prev, curr, rowIdx, arr) => {
+      const row = document.createElement('div');
+      row.classList.add('keyboard_row');
+      const currentRowButtons = buttons.slice(prev, curr+prev);
+      currentRowButtons.forEach((button, index) => {
+        this._addButtonsToTheRow(row, button);
+        keyboardWrap.appendChild(row);
+      });
+      return prev+curr;
     });
     document.querySelector('.main').appendChild(keyboardWrap);
-  }
+  },
+
+  _addButtonsToTheRow(row, button) {
+    const btnElement = document.createElement('button');
+    btnElement.innerHTML = button;
+    row.appendChild(btnElement);
+  },
 };
 
 window.addEventListener("DOMContentLoaded", function () {
