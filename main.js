68f5c313c0c003fd1b656d33eb8ff7d9de90b570
MedakD <timemedak@gmail.com>
2022-05-09 19:22:19 +0300

    feat: render buttons

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 const keyboard = {
-  layouts: {
-    eng: [
-      "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-      "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-      "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-      "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
-      "space"
-    ]
+  state: {
+    lang: 'eng' // TODO - add rus
+  },
+
+  getLayout() {
+    const layouts = {
+      rus: ['TODO'],
+      eng: [
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+        "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+        "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+        "space"
+      ]
+    };
+    return layouts[this.state.lang];
   },
 
   init() {
     const main = document.createElement('div');
     main.classList.add('main');
     document.body.appendChild(main);
+    this._addKeyboardButtons();
+  },
+
+  _addKeyboardButtons() {
+    const buttons = this.getLayout();
+    const keyboardWrap = document.createElement('div');
+    keyboardWrap.classList.add('keyboard_wrap');
+    buttons.forEach(button => {
+      const btnElement = document.createElement('button');
+      btnElement.innerHTML = button;
+      keyboardWrap.appendChild(btnElement);
+    });
+    document.querySelector('.main').appendChild(keyboardWrap);
   }
 };
 
